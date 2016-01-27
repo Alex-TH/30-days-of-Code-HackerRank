a52a8f188af93be589991dc08222cd1446e84414
Alex-TH <alex.trejoh@gmail.com>
2016-01-27 11:13:53 -0600

    added day 27

---
+++ day27.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    printf("5\n");
+    printf("4 3\n");
+    printf("0 -1 1 2\n");
+    printf("3 2\n");
+    printf("0 -1 2\n");
+    printf("5 4\n");
+    printf("0 -1 1 2 3\n");
+    printf("6 3\n");
+    printf("-3 -2 -1 0 1 2\n");
+    printf("7 3\n");
+    printf("-1 0 1 2 3 4 5\n");
+    return 0;
+}
+
